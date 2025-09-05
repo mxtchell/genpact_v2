cba0162a52c6a638edbe901716f57e1b7c7295b1
Joey Park <joey.park@answerrocket.com>
2025-09-05 14:44:18 -0400

    [SVCS-773] Fix to return and hierarchy filtering logic

--- analysis_class_overrides/insurance_utilities.py
+++ analysis_class_overrides/insurance_utilities.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 from ar_analytics.helpers.utils import SharedFn
 
 class InsuranceSharedFn(SharedFn):
@@ -9,13 +9,10 @@ class InsuranceSharedFn(SharedFn):
         return super().get_formatted_num(num, met_format, True, signed) 
     
 # Monkey patch to add metric hierarchy grouping logic
-def _filter_metric_hierarchy_by_groups(current_metric, metric_hierarchy, metric_hierarchy_groups) -> list:
+def _filter_metric_hierarchy_by_groups(current_metric, metric_hierarchy, metric_hierarchy_groups) -> List[dict]:
     """Filter metric_hierarchy based on metric_hierarchy_groups"""
-    if not current_metric:
-        return
-    
-    if not metric_hierarchy_groups or not metric_hierarchy:
-        return
+    if not current_metric or not metric_hierarchy_groups or not metric_hierarchy:
+        return metric_hierarchy
     
     target_group = None
     for group in metric_hierarchy_groups:
@@ -24,17 +21,19 @@ def _filter_metric_hierarchy_by_groups(current_metric, metric_hierarchy, metric_
             break
     
     if not target_group:
-        return
+        return metric_hierarchy
     
     # Filter metric_hierarchy to only include metrics from the target group
     filtered_hierarchy = []
     for item in metric_hierarchy:
         metric_name = item.get('metric')
-        if metric_name in target_group:
+        peers = item.get('peer_metrics') or []
+
+        # keep if the metric itself is in the group OR if any peers are in the group
+        if (metric_name in target_group) or any(peer in target_group for peer in peers):
             filtered_item = item.copy()
-            if 'peer_metrics' in filtered_item and filtered_item['peer_metrics']:
-                filtered_peers = [peer for peer in filtered_item['peer_metrics'] if peer in target_group]
-                filtered_item['peer_metrics'] = filtered_peers
+            if peers:
+                filtered_item['peer_metrics'] = [peer for peer in peers if peer in target_group]
             filtered_hierarchy.append(filtered_item)
     
     return filtered_hierarchy
