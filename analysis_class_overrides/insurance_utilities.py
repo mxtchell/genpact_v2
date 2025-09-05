3b61bfbef6b3346d1640266124b4ed5d60dc25c3
Joey Park <joey.park@answerrocket.com>
2025-09-05 12:34:39 -0400

    [SVCS-773] Metric hierarchy conditional groupings

--- analysis_class_overrides/insurance_utilities.py
+++ analysis_class_overrides/insurance_utilities.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from ar_analytics.helpers.utils import SharedFn
 
 class InsuranceSharedFn(SharedFn):
@@ -6,3 +7,34 @@ class InsuranceSharedFn(SharedFn):
 
     def get_formatted_num(self, num: float | int | str, met_format: str, pretty_num=False, signed=False):
         return super().get_formatted_num(num, met_format, True, signed) 
+    
+# Monkey patch to add metric hierarchy grouping logic
+def _filter_metric_hierarchy_by_groups(current_metric, metric_hierarchy, metric_hierarchy_groups) -> list:
+    """Filter metric_hierarchy based on metric_hierarchy_groups"""
+    if not current_metric:
+        return
+    
+    if not metric_hierarchy_groups or not metric_hierarchy:
+        return
+    
+    target_group = None
+    for group in metric_hierarchy_groups:
+        if current_metric in group:
+            target_group = group
+            break
+    
+    if not target_group:
+        return
+    
+    # Filter metric_hierarchy to only include metrics from the target group
+    filtered_hierarchy = []
+    for item in metric_hierarchy:
+        metric_name = item.get('metric')
+        if metric_name in target_group:
+            filtered_item = item.copy()
+            if 'peer_metrics' in filtered_item and filtered_item['peer_metrics']:
+                filtered_peers = [peer for peer in filtered_item['peer_metrics'] if peer in target_group]
+                filtered_item['peer_metrics'] = filtered_peers
+            filtered_hierarchy.append(filtered_item)
+    
+    return filtered_hierarchy
